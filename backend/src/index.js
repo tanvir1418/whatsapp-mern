2337bd96e0a39e13320e0d64a6a2be68d1873c48
tanvir1418 <tanvirahmed1418@gmail.com>
2024-01-27 09:10:34 +0600

    Handling uncaughtException, unhandledRejection, SIGTERM

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+const { error } = require("winston");
 const app = require("./app");
 const logger = require("./configs/logger.config");
 
@@ -6,6 +7,35 @@ const PORT = process.env.PORT || 8000;
 
 // console.log(process.env.NODE_ENV);
 
-app.listen(PORT, () => {
+let server;
+server = app.listen(PORT, () => {
   logger.info(`Server is running at: http://localhost:${PORT}`);
+  console.log("Process ID", process.pid);
+  // throw new Error("Error in server");
+});
+
+// handle server errors
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = () => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+// SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
 });
