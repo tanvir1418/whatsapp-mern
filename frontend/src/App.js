9b7ce285d4973465190758224dcb0af41ac95329
mdtanvir2627 <tanvir.netmow@gmail.com>
2024-01-30 13:08:32 +0600

    React Router Dom, Redux Toolkit, Persist Data, Initial Frontend Setup Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
 function App() {
-  return <div className="dark"></div>;
+  return (
+    <div className="dark">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/app/store.js
@@ -0,0 +1,35 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import createFilter from "redux-persist-transform-filter";
+
+// slices
+import userSlice from "../features/userSlice";
+
+// saveUserOnlyFilter
+const saveUserOnlyFilter = createFilter("user", ["user"]);
+
+// Persist Config
+const persistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["user"],
+  transforms: [saveUserOnlyFilter],
+};
+
+const rootReducer = combineReducers({
+  user: userSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
