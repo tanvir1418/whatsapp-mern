e1380492d622eb47f1b7be722b272939ad4ff201
mdtanvir2627 <tanvir.netmow@gmail.com>
2024-01-31 18:08:17 +0600

    Authentication Backend - Register User with Validation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,17 @@
+const { createUser } = require("../services/auth.service");
+
 const register = async (req, res, next) => {
   try {
-    res.send(req.body);
+    const { name, email, picture, status, password } = req.body;
+    const newUser = await createUser({
+      name,
+      email,
+      picture,
+      status,
+      password,
+    });
+
+    res.json(newUser);
   } catch (error) {
     next(error);
   }
