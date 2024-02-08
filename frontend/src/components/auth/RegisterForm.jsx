637321acac96cd1e7ea7e60549d38127ab4e89f5
mdtanvir2627 <tanvir.netmow@gmail.com>
2024-02-08 16:46:00 +0600

    Upload Image to Cloudinary

--- frontend/src/components/auth/RegisterForm.jsx
+++ frontend/src/components/auth/RegisterForm.jsx
@@ -6,8 +6,11 @@ import AuthInput from "./AuthInput";
 import { useDispatch, useSelector } from "react-redux";
 import { PulseLoader } from "react-spinners";
 import { Link, useNavigate } from "react-router-dom";
-import { registerUser } from "../../features/userSlice";
+import { changeStatus, registerUser } from "../../features/userSlice";
 import Picture from "./Picture";
+import axios from "axios";
+const cloud_name = process.env.REACT_APP_CLOUD_NAME;
+const cloud_secret = process.env.REACT_APP_CLOUD_SECRET;
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
@@ -26,10 +29,40 @@ const RegisterForm = () => {
   });
 
   const onSubmit = async (data) => {
-    let response = await dispatch(registerUser({ ...data, picture: "" }));
-    if (response.payload.user) {
-      navigate("/");
+    dispatch(changeStatus("loading"));
+    if (picture) {
+      //upload to cloudinary and then register user
+      await uploadImage().then(async (response) => {
+        let res = await dispatch(
+          registerUser({ ...data, picture: response.secure_url })
+        );
+        if (res?.payload?.user) {
+          navigate("/");
+        }
+      });
+    } else {
+      let res = await dispatch(registerUser({ ...data, picture: "" }));
+      if (res?.payload?.user) {
+        navigate("/");
+      }
     }
+  };
+
+  const uploadImage = async () => {
+    let formData = new FormData();
+    formData.append("upload_preset", cloud_secret);
+    formData.append("file", picture);
+
+    // Upload file to a specific folder
+    // formData.append("folder", "myFolder");
+
+    // Cloudinary Upload API reference
+    const { data } = await axios.post(
+      `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+      formData
+    );
+    console.log(data);
+    return data;
   };
 
   return (
