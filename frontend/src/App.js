5bea5db767b3e9b1b69ec87e0eb6f23ba215dab6
tanvir1418 <tanvirahmed1418@gmail.com>
2024-02-08 22:37:54 +0600

    Protected Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 // Pages
 import Home from "./pages/home";
@@ -7,16 +12,25 @@ import Register from "./pages/register";
 import { useSelector } from "react-redux";
 
 function App() {
-  const { user } = useSelector((state) => ({ ...state }));
-  console.log(user);
+  const { user } = useSelector((state) => state.user);
+  const { access_token: token } = user;
 
   return (
     <div className="dark">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={token ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!token ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/register"
+            element={!token ? <Register /> : <Navigate to="/" />}
+          />
         </Routes>
       </Router>
     </div>
